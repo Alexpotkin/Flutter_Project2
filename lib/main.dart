bdc62588afb10445db93a181038fb99f52b6ec2c
AlexPotkin <Alexkkm78@gmail.com>
2021-07-23 02:56:43 +0300

    crash

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
-import 'package:first_flutter_project/widgets/app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  final app = App();
-  runApp(app);
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('MyAPP', style: TextStyle(
+        fontSize: 15,
+        color: Colors.red,
+        fontFamily: 'Arial'
+      ),),
+    );
+  }
 }
